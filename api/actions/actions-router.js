65b0633c0c71f1236fd2a91d356b9477b0c1e073
unknown <dshipuk@yahoo.com>
2022-05-12 20:32:42 -0700

    Sending Post req to /api/actions with required data now returns what was submitted and sending an put to /api/actions/:id updates given ids info

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -11,4 +11,36 @@ router.get("/", (req, res) => {
         })
 })
 
+router.get("/:id", (req, res) => {
+    Action.get(req.params.id)
+        .then( result => {
+            result
+            ? res.json(result)
+            : res.status(404).json()
+        })
+        .catch( () => res.status(404).json())
+})
+
+router.post("/", (req, res) => {
+    Action.insert(req.body)
+        .then( result => {
+            res.json(result)
+        })
+        .catch( () => res.status(400).json())
+})
+
+router.put("/:id", (req, res) => {
+    Action.update(req.params.id, req.body)
+        .then( result => {
+            const { notes, description, project_id } = req.body
+            
+            notes && description && project_id
+            ? res.status(201).json(result)
+            : res.status(400).json()
+        })
+        .catch( () => {
+            res.status(400).json()
+        })
+})
+
 module.exports = router;
