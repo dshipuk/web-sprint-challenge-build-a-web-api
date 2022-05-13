08bf0bde51bc1003b0425f5f451743116d67da14
unknown <dshipuk@yahoo.com>
2022-05-12 18:06:17 -0700

    SetUp Server in Index.js, Setup Start script and Server Script in package.json, Installed nodemon, dotenv, Setup Handling if route requested doesn't exist, setup projectsRouter use in server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const projectsRouter = require("./projects/projects-router");
 
+// Configure your server here
+server.use(express.json());
+
+
+// Build your actions router in /api/actions/actions-router.js
+
+
+// Build your projects router in /api/projects/projects-router.js
+server.use("/api/projects", projectsRouter);
+
+
+
+// Handling if No Route Provided
+server.use("*", (res, req) => {
+    res.statusCode(404).json({
+        message: "Not Found"
+    })
+})
+
+
+// Do NOT `server.listen()` inside this file!
 module.exports = server;
